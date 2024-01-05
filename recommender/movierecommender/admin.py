ab6e7f5f9c3a9a09f6419887ad794734bd7bfe42
Joveth-Dev <jovethespiritu.developer@gmail.com>
2024-01-05 19:25:53 +0800

    Finished

--- recommender/movierecommender/admin.py
+++ recommender/movierecommender/admin.py
@@ -6,9 +6,19 @@ from .models import Movie
 
 
 class MovieAdmin(admin.ModelAdmin):
-    fields = ['imdb_id', 'genres', 'original_title', 'overview', 'watched']
-    list_display = ('original_title', 'genres', 'release_date', 'watched')
-    search_fields = ['original_title', 'overview']
+    actions = ["mark_as_watched"]
+    fields = ["imdb_id", "genres", "original_title", "overview", "watched"]
+    list_display = ("original_title", "genres", "release_date", "watched")
+    search_fields = ["original_title", "overview"]
+
+    @admin.action(description="Mark selected as Watched")
+    def mark_as_watched(self, request, queryset):
+        updated_count = queryset.update(watched=True)
+        self.message_user(
+            request,
+            f"{updated_count} were successfully marked as watched.",
+            "success",
+        )
 
 
 admin.site.register(Movie, MovieAdmin)
